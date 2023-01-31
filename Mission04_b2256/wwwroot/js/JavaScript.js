d34804303a1b6959039102af9dc4ece03fb5f0a1
bruyndecker <bruyn.decker@byu.edu>
2023-01-31 04:50:15 -0700

    FIxed some stuff

--- Mission04_b2256/wwwroot/js/JavaScript.js
+++ Mission04_b2256/wwwroot/js/JavaScript.js
@@ -1,11 +1,21 @@
-﻿$("#btnSend").click(function ()  {
+﻿// When the "Send Message" button is clicked
+$("#btnSend").click(function () {
+    // Get the values from the form, convert them to floats, and multiply them by the percentage
+    // Also assign them to the variables we'll use to calculate the final grade
     var assignments = parseFloat($("#textAsn").val()) * 0.5;
     var groupProject = parseFloat($("#gp").val()) * 0.1;
     var quizzes = parseFloat($("#quizzes").val()) * 0.1;
     var midtermExam = parseFloat($("#mExam").val()) * 0.1;
     var finalExam = parseFloat($("#fExam").val()) * 0.1;
     var intex = parseFloat($("#intex").val()) * 0.1;
+
+    // Calculate the final grade using the variables we made above
     var finalGrade = assignments + groupProject + quizzes + midtermExam + finalExam + intex;
+
+    // Initialize the letter grade variable
+    var letterGrade;
+
+    // This if statement determines the letter grade based on the final grade percentage
     if (finalGrade >= 94) {
         letterGrade = "A";
     } else if (finalGrade >= 90) {
@@ -32,5 +42,6 @@
         letterGrade = "E";
     }
 
+    // Put the calculated letter grade in the textarea in the form
     $("textarea").val("Your final grade is: " + letterGrade);
 })
